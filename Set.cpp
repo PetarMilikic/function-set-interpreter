6678c2743b2ad8cb4df621db3cf964508329c834
PetarMilikic <74273842+PetarMilikic@users.noreply.github.com>
2021-08-30 20:58:40 +0200

    Update Set.cpp

--- Set.cpp
+++ Set.cpp
@@ -1,5 +1,3 @@
-
-
 #include<iostream>
 #include<cstdlib>
 #include<set>
@@ -10,55 +8,57 @@ Set Set:: operator +(const Set& s)const
 {
     //unija
     Set skup;
-    set<double>::const_iterator it1=begin();
-    for(;it1!=end();it1++)
+    set<double>::const_iterator it1= begin();
+    for( ; it1!=end(); it1++)
         skup.insert(*it1);
     it1=s.begin();
-    for(;it1!=s.end();it1++)
+    for( ; it1 != s.end(); it1++)
         skup.insert(*it1);
     return skup;
 }
+
 Set Set::operator * (const Set& s)const
 {
     //presek
     Set skup;
-    set<double>::const_iterator it1=begin();
-    for(;it1!=end();it1++)
+    set<double>::const_iterator it1 = begin();
+    for( ; it1 != end(); it1++)
     {
-        if(s.find(*it1)!=s.end())
+        if(s.find(*it1) != s.end())
             skup.insert(*it1);
     }
     return skup;
 }
 
-Set Set::operator -(const Set& s)const
+Set Set::operator - (const Set& s)const
 {
     Set skup;
-    set<double>::const_iterator it1=begin();
-    for(;it1!=end();it1++)
+    set<double>::const_iterator it1 = begin();
+    for(; it1 != end(); it1++)
     {
-        if(s.find(*it1)==s.end())
+        if(s.find(*it1) == s.end())
             skup.insert(*it1);
     }
     return skup;
 }
+
 unsigned Set::card()const
 {
     return size();
 }
+
 bool Set::pripada (const int& d)const
 {
-    set<double>::const_iterator it1=find(d);
-    if(it1==end())
+    set<double>::const_iterator it1 = find(d);
+    if(it1 == end())
         return false;
     return true;
 
 }
+
 set<Set> Set::partitivni ()const
 {
-  
-
-     set<Set> result;
+  set<Set> result;
   // ubacujemo prazan skup
   result.insert(Set());
   set<Set>::iterator i = result.begin();
@@ -73,40 +73,37 @@ set<Set> Set::partitivni ()const
   }
   return result;
 }
+
 void Set::ispisi(ostream& izlaz)const
 {
-    unsigned brojac=0;
-    izlaz<<"{";
-    set<double>::const_iterator it1=begin();
-    for(;it1!=end();it1++)
+    unsigned brojac=  0;
+    izlaz << "{";
+    set<double>::const_iterator it1 = begin();
+    for(; it1 != end(); it1++)
     {
         brojac++;
-        if(brojac==size())
-            izlaz<<*it1;
+        if(brojac == size())
+            izlaz << *it1;
         else
         {
-            izlaz<<*it1<<", ";
+            izlaz<< *it1 <<", ";
         }
     }
     izlaz<<"}";
-
 }
 
-
-
 bool Set::operator < (const Set& s) const {
-  if (size()<s.size())
+  if (size() < s.size())
     return true;
-  if (s.size()<size())
+  if (s.size() < size())
     return false;
-  for (set<double>::iterator i = begin(), j = s.begin(); i!=end(); i++, j++)
-    if (*i<*j)
+  for (set<double>::iterator i = begin(), j = s.begin(); i != end(); i++, j++)
+    if (*i < *j)
       return true;
-    else if (*i>*j)
+    else if (*i > *j)
       return false;
   return false;
 }
-
 
 //proba
 /*
@@ -130,14 +127,11 @@ int main()
         cout<<x;
     }
     cout<<"}\n";
-    
-    
-    
     return 0;
 }*/
+
 ostream& operator << (ostream& ostr,const Set& c)
 {
     c.ispisi(ostr);
     return ostr;
 }
-
