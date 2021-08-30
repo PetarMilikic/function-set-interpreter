e26e12ce70c14e5c97c1a08ccca80c0ab92feb26
PetarMilikic <74273842+PetarMilikic@users.noreply.github.com>
2021-08-30 21:03:20 +0200

    Update Set.hpp

--- Set.hpp
+++ Set.hpp
@@ -5,18 +5,18 @@
 #include<cstdlib>
 #include<set>
 using namespace std;
+
 class Set:public set<double>
 {
-    
 public:
     Set() {}
-    Set operator +(const Set& s)const;
+    Set operator + (const Set& s)const;
     Set operator * (const Set& s)const;
-    Set operator -(const Set& s)const;
+    Set operator - (const Set& s)const;
     unsigned card()const;
     bool pripada (const int& d)const;
     set<Set> partitivni ()const;
-    void ispisi(ostream& izlaz)const;
+    void ispisi (ostream& izlaz)const;
     bool operator < (const Set& s)const;
 };
 
